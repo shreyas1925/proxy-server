a04cfe2355772a4d0ccfe4fd6404cadd2cf3debc
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-10-27 22:38:11 +0530

    ✅ fetching news api data

---
+++ public/script.js
@@ -0,0 +1,38 @@
+const row = document.querySelector("#row");
+
+const apicall = async () => {
+  const res = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=4275f573e8ba4f06be195172d387666a",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+
+  const data = await res.json();
+
+  //   console.log(data);
+
+  shownews(data.articles);
+  console.log(data.articles, "ss");
+};
+
+const shownews = (data) => {
+  data.map((data) => {
+    const card = `<div class="col-sm col-lg-4 col-md-4 mb-3">
+        <div class="card" style="height:500px">
+          <img class="card-img-top" src="${data.urlToImage}" alt="Card image cap" />
+          <div class="card-body d-flex flex-grow flex-column card_body">
+            <h5 class="card-title text-dark">${data.author}</h5>
+            <p class="card-text text-dark">
+              ${data.description}
+            </p>
+            </div>
+            <a href="#" class="btn btn-primary ">Content</a>
+        </div>
+        </div>`;
+    row.insertAdjacentHTML("afterbegin", card);
+  });
+};
