254c68baaee34cc7a5c0225c1c18b3d5789faad8
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-10-31 00:05:36 +0530

    started with backend server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes/news");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/api", routes);
+// whenever I go to /api i would require routes/news
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const row = document.querySelector("#row");
+const container = document.querySelector(".container");
 
 const apicall = async () => {
   const res = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`,
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=`,
     {
       method: "GET",
       headers: {
@@ -21,18 +22,43 @@ const apicall = async () => {
 
 const shownews = (data) => {
   data.map((data) => {
-    const card = `<div class="col-sm col-lg-4 col-md-4 mb-3">
+    const card = `
+    <div class="col-sm col-lg-4 col-md-6 mb-3">
         <div class="card" style="height:500px">
-          <img class="card-img-top" src="${data.urlToImage}" alt="Card image cap" style="height:200px"/>
+          <img class="card-img-top" src="${data.urlToImage}" alt="Card image cap" style="height:240px"/>
           <div class="card-body d-flex flex-grow flex-column card_body overflow-none">
             <h5 class="card-title text-dark font-weight-bold">${data.author}</h5>
             <p class="card-text text-dark">
               ${data.description}
             </p>
             </div>
-            <a href="#" class="btn btn-primary ">Content</a>
+            <a href="#" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">Content</a>
         </div>
-        </div>`;
+    </div>`;
+
+    const modal = `
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title text-info font-weight-bold" id="exampleModalLabel">${data.title}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body text-dark">
+              ${data.content}
+            </div>
+            <div class="modal-footer">
+            <p class="font-weight-bold text-primary">🚀 Source ${data.source.name}</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+        `;
+
     row.insertAdjacentHTML("afterbegin", card);
+    container.insertAdjacentHTML("beforeend", modal);
   });
 };
