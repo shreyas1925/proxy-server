649ecb2c6b3c1542e2e9c99627ce13840200827a
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-03 23:47:21 +0530

    added line

--- routes/news.js
+++ routes/news.js
@@ -12,21 +12,20 @@ const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
 router.get("/", async (req, res) => {
   // res.json({ success: true });
-  try{
+  try {
+    // const params = new URLSearchParams({
+    //   [API_KEY_NAME] : API_KEY_VALUE,
+    // })
 
-  // const params = new URLSearchParams({
-  //   [API_KEY_NAME] : API_KEY_VALUE,
-  // })
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+    });
 
-const params = new URLSearchParams({
-  [API_KEY_NAME] : API_KEY_VALUE
-})
-
-  const apiRes = await needle("get", `${API_BASE_URL}&${params}`);
-  const data = apiRes.body;
-  res.status(200).json(data);
-  } catch(err){
-    res.status(500).json({err})
+    const apiRes = await needle("get", `${API_BASE_URL}?${params}`);
+    const data = apiRes.body;
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ err });
   }
 });
 
